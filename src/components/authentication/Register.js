189230ae4d8a0f74d81f69318363573b7f81e628
Aleksander <mrhdolek14@gmail.com>
2021-03-01 21:51:58 +0100

    Update Register.js

--- src/components/authentication/Register.js
+++ src/components/authentication/Register.js
@@ -26,7 +26,7 @@ const Register = ({ getAuth }) => {
       <h1 className="text-gray-200 text-center text-4xl mb-5">Register</h1>
       <form onSubmit={onSubmit}>
         <InputField
-          label="Email"
+          label="test"
           type="email"
           name="Email"
           id="Email"
