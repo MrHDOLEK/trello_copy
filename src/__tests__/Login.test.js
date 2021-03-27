ddb7ac9fc91e7a2e12087db10cbd63b5a2cd8862
shiragaira <shiragaira@gmail.com>
2021-03-27 16:45:14 +0100

    Adding tests

--- src/__tests__/Login.test.js
+++ src/__tests__/Login.test.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 
-import Login from "../components/authentication/Login";
+import AuthenticationPage from "../components/authentication/AuthenticationPage";
 import store from "../store";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 
 describe("<Login> tests", () => {
-  beforeEach(() =>
+  beforeEach(() => {
     render(
       <Provider store={store}>
         <Router>
-          <Login option="login" />
+          <AuthenticationPage option="login" />
         </Router>
       </Provider>
-    )
-  );
+    );
+  });
 
   test("contain 'Login' title", () => {
     expect(screen.getByText(/login/i)).toBeInTheDocument();
@@ -28,5 +29,15 @@ describe("<Login> tests", () => {
 
   test("contain password input field", () => {
     expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
+  });
+
+  test("contain button", () => {
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+
+  test("contain small text", () => {
+    expect(screen.getByText(/you don't have an account/i)).toBeInTheDocument();
+    expect(screen.getByRole("link")).toBeInTheDocument();
+    expect(screen.getByRole("link")).toHaveTextContent(/sign up!/i);
   });
 });
