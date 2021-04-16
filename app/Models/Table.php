f92100697ddf393c167b718ff35c2783bc108562
Aleksander <mrhdolek14@gmail.com>
2021-04-16 16:11:48 +0200

    Update table
    During create table so create example task and card

--- app/Models/Table.php
+++ app/Models/Table.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use MongoDB\Driver\Exception\ExecutionTimeoutException;
 
 class Table extends Model
 {
@@ -88,20 +90,100 @@ class Table extends Model
 
     public function createTable(string $name, string $name_user, int $creator_id)
     {
-        return $table = Table::create([
-            'users' => json_encode([$name_user]),
-            'name' => $name,
-            'is_visible' => 1,
-            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-            'creator_id' => $creator_id,
-            'theme_id' => 1,
-        ]);
+        try {
+            $table = Table::create([
+                'users' => json_encode([$name_user]),
+                'name' => $name,
+                'is_visible' => 0,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'creator_id' => $creator_id,
+                'theme_id' => 1,
+            ]);
+            $card = Card::create([
+                'card_name' => 'To do',
+                'card_content' => json_encode('To do:'),
+                'card_type' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'table_id' => $table->id
+            ]);
+            DB::table('card_table')->insert([
+                'card_id' => $card->id,
+                'table_id' => $table->id
+            ]);
+            $task = Task::create([
+                'task_name' => 'Fix car',
+                'task_content' => json_encode('Broken battery'),
+                'task_type' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'card_id' => $card->id,
+            ]);
+            DB::table('task_card')->insert([
+                'card_id' => $card->id,
+                'task_id' => $task->id
+            ]);
+
+            $card = Card::create([
+                'card_name' => 'Doing - now',
+                'card_content' => json_encode('Doing - now:'),
+                'card_type' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'table_id' => $table->id
+            ]);
+            DB::table('card_table')->insert([
+                'card_id' => $card->id,
+                'table_id' => $table->id
+            ]);
+            $task = Task::create([
+                'task_name' => 'Wash the dog',
+                'task_content' => json_encode('Wash the dog'),
+                'task_type' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'card_id' => $card->id,
+            ]);
+            DB::table('task_card')->insert([
+                'card_id' => $card->id,
+                'task_id' => $task->id
+            ]);
+
+            $card = Card::create([
+                'card_name' => 'Done',
+                'card_content' => json_encode('Done'),
+                'card_type' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'table_id' => $table->id
+            ]);
+            DB::table('card_table')->insert([
+                'card_id' => $card->id,
+                'table_id' => $table->id
+            ]);
+            $task = Task::create([
+                'task_name' => 'Make dinner',
+                'task_content' => json_encode('spaghetti and soup'),
+                'task_type' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'card_id' => $card->id,
+            ]);
+            DB::table('task_card')->insert([
+                'card_id' => $card->id,
+                'task_id' => $task->id
+            ]);
+
+            return true;
+        } catch (Execution $e) {
+            return null;
+        }
     }
 
     public function updateTable(int $id, int $is_visible, string $table_name, int $user_id)
     {
-        $creator_id = Table::where('id',$id)->where('creator_id',$user_id)->firstOrFail();
+        $creator_id = Table::where('id', $id)->where('creator_id', $user_id)->firstOrFail();
         if (!($creator_id->creator_id == $user_id)) {
             return null;
         }
@@ -118,7 +200,7 @@ class Table extends Model
 
     public function deleteTable(int $id, int $user_id)
     {
-        $creator_id = Table::where('id',$id)->where('creator_id',$user_id)->firstOrFail();
+        $creator_id = Table::where('id', $id)->where('creator_id', $user_id)->firstOrFail();
         if (!($creator_id->creator_id == $user_id)) {
             return null;
         }
