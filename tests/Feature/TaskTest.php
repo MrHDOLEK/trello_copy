a20cef706e3a6a2a2ef078e69ae5ab282749bdcd
Aleksander <mrhdolek14@gmail.com>
2021-04-23 18:25:27 +0200

    Create tests

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Card;
+use App\Models\Table;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,10 +15,41 @@ class TaskTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function testCreateTaskInCard()
     {
-        $response = $this->get('/');
-
+        $token = $this->createToken();
+        $table = Table::latest('id')->first();
+        $card = Card::where('table_id',$table['id'])->latest('id')->first();
+        $response = $this->postJson('/api/v1/manage/tasks?id='.$card['id'], [
+            'task_name' => $this->faker->name(),
+            'task_content' => $this->faker->regexify('[A-Za-z0-9]{20}')
+        ],[
+            'Authorization' => 'Bearer '.$token
+        ]);
+        $response->assertStatus(200);
+    }
+    public function testUpdateTaskInCard()
+    {
+        $token = $this->createToken();
+        $table = Table::latest('id')->first();
+        $card = Card::where('table_id',$table['id'])->latest('id')->first();
+        $response = $this->putJson('/api/v1/manage/tasks?id='.$card['id'], [
+            'task_name' => $this->faker->name(),
+            'task_content' => $this->faker->regexify('[A-Za-z0-9]{20}')
+        ],[
+            'Authorization' => 'Bearer '.$token
+        ]);
+        $response->assertStatus(200);
+    }
+    public function testDeleteTaskInCard()
+    {
+        $token = $this->createToken();
+        $table = Table::latest('id')->first();
+        $card = Card::where('table_id',$table['id'])->latest('id')->first();
+        $response = $this->deleteJson('/api/v1/manage/tasks?id='.$card['id'], [
+        ],[
+            'Authorization' => 'Bearer '.$token
+        ]);
         $response->assertStatus(200);
     }
 }
